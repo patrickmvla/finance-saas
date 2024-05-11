8d5d4ec9a4faa37d9587c93c035f354990b13d12
Patrick Mvula <mvlapatrick@gmail.com>
2024-05-11 17:14:32 +0200

    hono setup

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return <div>{children}</div>;
 };
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
-    return ( 
-        <div>
-            <UserButton/>
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+};
+
 export default DashboardPage;
