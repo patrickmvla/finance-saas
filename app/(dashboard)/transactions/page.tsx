227772e9f2bc065a3faf8ec05ed31614eba76fb0
Patrick Mvula <mvlapatrick@gmail.com>
2024-06-05 19:33:56 +0200

    transactions CSV import

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -10,8 +10,35 @@ import { useBulkDeleteTransactions } from "@/modules/transactions/api/use-bulk-d
 import { useGetTransactions } from "@/modules/transactions/api/use-get-transactions";
 import { useNewTransaction } from "@/modules/transactions/hooks/use-new-transaction";
 import { Loader2, Plus } from "lucide-react";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionsPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
   const newTransaction = useNewTransaction();
   const deleteTransactions = useBulkDeleteTransactions();
   const transactionsQuery = useGetTransactions();
@@ -37,6 +64,18 @@ const TransactionsPage = () => {
     );
   }
 
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
@@ -44,10 +83,17 @@ const TransactionsPage = () => {
           <CardTitle className="text-xl line-clamp-1">
             Transactions History
           </CardTitle>
-          <Button size="sm" onClick={newTransaction.onOpen}>
-            <Plus className="size-4 mr-2" />
-            Add new
-          </Button>
+          <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
+            <Button
+              size="sm"
+              onClick={newTransaction.onOpen}
+              className="w-full lg:w-auto"
+            >
+              <Plus className="size-4 mr-2" />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
---
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -0,0 +1,57 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  columnIndex: number;
+  onChange: (columnIndex: number, value: string | null) => void;
+  selectedColumns: Record<string, string | null>;
+};
+
+const options = ["amount", "payee", "date"];
+
+export const TableHeadSelect = ({
+  columnIndex,
+  onChange,
+  selectedColumns,
+}: Props) => {
+  const currentSelection = selectedColumns[`column_${columnIndex}`];
+  return (
+    <Select
+      value={currentSelection || ""}
+      onValueChange={(value) => onChange(columnIndex, value)}
+    >
+      <SelectTrigger
+        className={cn(
+          "focus:ring-offset-0 focus:ring-transparent outline-none border-none bg-transparent capitalize",
+          currentSelection && "text-blue-500"
+        )}
+      >
+        <SelectValue placeholder="Skip" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="skip">Skip</SelectItem>
+        {options.map((option, index) => {
+          const disabled =
+            Object.values(selectedColumns).includes(option) &&
+            selectedColumns[`column_${columnIndex}`] !== option;
+          return (
+            <SelectItem
+              key={index}
+              value={option}
+              disabled={disabled}
+              className="capitalized"
+            >
+              {option}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
+};
